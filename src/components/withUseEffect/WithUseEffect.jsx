bf34c88b11b5b684e7283aed52734aa52049b196
Tony Heiser <tonyhzer@gmail.com>
2024-05-29 19:52:13 +0200

    290524-1952

---
+++ src/components/withUseEffect/WithUseEffect.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+import "./with-use-effect.css";
+
+export default function Namae() {
+  const [type, setType] = useState("users");
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${type}/1`)
+      .then(response => response.json())
+      .then(json => setData(json))
+  }, [type])
+
+  return (
+    <>
+      <h1>
+        Source: {type}
+      </h1>
+      <button onClick={() => setType("users")} className="namae__btn">Users</button>
+      <button onClick={() => setType("todos")} className="namae__btn">Todos</button>
+      <button onClick={() => setType("posts")} className="namae__btn">Posts</button>
+
+      <pre>{JSON.stringify(data.address, null, 2)}</pre>
+    </>
+  )
+}
