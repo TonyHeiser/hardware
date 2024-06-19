7c93de27e05e1838542bf3ba1a96e3a826f3dd09
Tony Heiser <tonyhzer@gmail.com>
2024-06-19 16:29:45 +0200

    try catch was added in element

--- src/components/periodic-table/PeriodicTableElement.jsx
+++ src/components/periodic-table/PeriodicTableElement.jsx
@@ -7,21 +7,25 @@ export default function ChemicalElement({element}) {
 
   const [expandInfo, setExpandInfo] = useState(false);
 
-  return (
-    <td>
-      <button className={`periodic-table-20__btn ${element.group}`} onClick={() => setExpandInfo(true)}>
-        <div className="element20-info__up">
-          <span className="element20__char span-text">{element.char}</span>
-          <span className="element20__number span-text">{element.number}</span>
-        </div>
-        <div className="element20-info__center">
-          <span className="element20__atomic-mass span-text">{element.atomicMass}</span>
-          <span className="element20__electronegativity span-text">{element.electronegativity}</span>
-        </div>
-        <span className={`element20__name span-text ${element.fstyle}` }>{element.nameEng}</span>
-      </button>
-      { expandInfo && <ElementExpand element={element} closeExpand={setExpandInfo}/> }
-    </td>
-  )
+  try {
+    return (
+      <td>
+        <button className={`periodic-table-20__btn ${element.group}`} onClick={() => setExpandInfo(true)}>
+          <div className="element20-info__up">
+            <span className="element20__char span-text">{element.char}</span>
+            <span className="element20__number span-text">{element.number}</span>
+          </div>
+          <div className="element20-info__center">
+            <span className="element20__atomic-mass span-text">{element.atomicMass}</span>
+            <span className="element20__electronegativity span-text">{element.electronegativity}</span>
+          </div>
+          <span className={`element20__name span-text ${element.fstyle}` }>{element.nameEng}</span>
+        </button>
+        { expandInfo && <ElementExpand element={element} closeExpand={setExpandInfo}/> }
+      </td>
+    )
+  } catch {
+    return <span className="element20__error-text">Error</span>
+  }
 }
 
